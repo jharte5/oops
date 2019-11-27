4f515b2f8ab155ab1c9d6e4579df207b4a7987e7
jharte5 <jonathan.harte@codeimmersives.com>
2019-11-27 14:15:20 -0500

    inital commit

--- main.js
+++ main.js
@@ -1,7 +1,34 @@
 const Stack = function() {
+  return {
+    // firstName: first,
+    // lastName: last,
+    // age: age,
+    items: [],
+      add: function (firstName) {
+        this.items.push(firstName)
+      },
+      remove: function () {
+        let removed = this.items.splice(this.items.length-1, 1)[0]
+        return removed
+      },
+      peek: function () {
+        return items.unshift()
+      },
+
+  }
 }
 
 const Queue = function() {
+  return {
+    items:[],
+      add: function (firstName) {
+        this.items.push(firstName)
+      },
+      remove: function () {
+        let removed = this.items.splice(this.items.length-1, 1)[0]
+        return removed
+      }
+  }
 }
 
 module.exports = {
